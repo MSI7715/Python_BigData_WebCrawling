f4ed1c4908c54c994bc46c5c8980cdf14623fedb
Data_WH <git@example.com>
2025-12-07 15:59:30 +0800

    BigData_Lesson1_PM

--- FirstClass.py
+++ FirstClass.py
@@ -44,7 +44,53 @@ print( "100" + "1" ) # 字串串接
 # print( hello * "2" ) # 字串重複
 print( hello * 2 )  # 字串重複
 
-#
+# 輸入與轉型
 input1 = int( input("請輸入整數: ") )   # input()輸入的資料型態是字串，需要轉型成整數int
 input2 = input( "請輸入整數: " )
 print( input1, int(input2) )
+
+# 索引 index
+print( hello[0] ) # 從0開始，取得字串的第一個字元 (索引從0開始)，中括弧內可以運算也可以是變數
+print( hello[1:7] ) # 前含後不含
+print( hello[-1] ) # 取得字串的最後一個字元 (負索引從-1開始)，倒數
+print( hello[ : -3 ] ) #從頭
+print( hello[ 3 : ] ) #到尾
+print( hello[ : ] ) #全部
+print( hello[ 1:-3:3 ] )   # [開頭(含)：結尾(不含)：項差(預設為1，可為負數)]
+#print( hello[20] )
+
+# list 清單 []
+list1 = [ 1, 2, 3, '4', 5, [ 6, '7' ] ]
+print( list1 + [ '8, 9' ], 10 ) # 清單串接
+print( list1 * 3 ) # 清單重複
+print( list1[ -1 ] ) # 取得清單的最後一個元素
+print( list1[ -1 ][ 1 ] ) # 按照順序選取
+print( len( list1 ) )  # 取得清單長度
+# 賦值取代
+list1[ 0 ] = 0
+print( list1 )
+print( [ 'a', 'b', 'c', 1, 2, 3 ][ 4 ] )
+print( [0][0] )
+
+# Dictionary 字典 { key: value, key: value } key只能是數字或字串
+movie = {
+    'name': '鋼鐵人3',
+    '上映日': { '年': 2013, '月': 5, '日': 3 },
+    '演員': [ '小勞勃·道尼', '葛妮絲·派特洛', '唐·奇鐸' ],
+    '片長': 130
+}
+print( movie['name'] )
+print( movie['上映日']['年'] )
+movie[ '時長' ] = '2小時10分'  # 賦值更新字典內容
+print( movie )
+movie[ '導演' ] = '沙恩·布萊克'  # 賦值新增字典內容
+print( movie )
+# 字典如有重複內容會覆蓋
+movie = {
+    'name': '鋼鐵人3',
+    '上映日': { '年': 2013, '月': 5, '日': 3 },
+    '演員': [ '小勞勃·道尼', '葛妮絲·派特洛', '唐·奇鐸' ],
+    '片長': 130,
+    '演員': [ '小勞勃·道尼', '葛妮絲·派特洛', '唐·奇鐸', '陳聲翊' ]
+}
+print( movie )
--- practice1.py
+++ practice1.py
@@ -14,4 +14,29 @@ retiredAge = 65
 retiredYear = birth_year + retiredAge + 1911
 print("您的退休年份為", retiredYear)
 
-# 第三個練習 -
+# 第三個練習 - 字串選取
+alp = 'abcdefghijklmnopqrstuvwxyz'
+# 輸出結果為：'cfilo'
+alp_pick = alp[ 2 : 15 : 3 ]
+print( alp_pick )
+# 輸出結果為：'z~a'
+alp_reverse = alp[ ::-1 ]
+print( alp_reverse )
+# 輸出結果為：'xsni'
+alp_pick_reverse = alp[ -3 : 7 : -5 ]
+print( alp_pick_reverse )
+# 補充 - 連續選取
+print( alp[::-1][5:20][:10][9] )
+
+# 第四個練習 - list清單操作
+list_m = [ "Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec" ]
+list_d = [ 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 ]
+# 輸出結果為： 'x月有 y days.'
+month_index = int( input("請輸入月份數字(1-12): ") ) - 1
+print( list_m[ month_index ] + " has", list_d[ month_index ], "days." )
+
+# 第五個練習 - 字典索引
+dict=[ "a","b,c","d","e",{ "f":[ "g","h" ],"i":"j","k,l":{ "m":[ "n","o","p" ] },"q":"r" },"s",[ "t,u","v","w" ],"x","y,z" ]
+# 索引出'python'
+dict_result = dict[4]["k,l"]["m"][2] + dict[4]["k,l"]["m"][0] + dict[4]["k,l"]["m"][1] + dict[6][0][2] + dict[6][1][0] + dict[6][2][1]
+print( dict_result )
