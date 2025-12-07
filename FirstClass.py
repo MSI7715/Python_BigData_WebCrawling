242de40e35d3d9f5f6264fc86320e5311371df21
Data_WH <git@example.com>
2025-12-07 13:18:16 +0800

    BigData_Lesson1_AM

--- FirstClass.py
+++ FirstClass.py
@@ -19,3 +19,32 @@ help("keywords")  # 查看Python保留字(不能用來當變數名稱)
 # 變數命名規則: 只能包含字母、數字、底線，且不能以數字開頭，區分大小寫，不能使用保留字，而且只能使用_符號
 # 3c = 5
 # hi! = 7
+
+
+# 變數運算      程式碼運行順序：由上至下，由左至右，由內至外，右側運算完才賦值至左側變數
+a = 0
+a = a + 1
+print( a )
+a += 1  # 簡寫
+print( a )
+a *= 2  # 簡寫
+print( a )
+
+a1, b1 = 1, 2  # 多重賦值
+a1, b1 = b1, a1  # 交換變數值
+print( a1, b1 )
+print( type( 4 * 1 ) ) # 整數 int integer
+print( type( 4 / 1 ) ) # 浮點數 float
+print( type( "4 / 1" ) ) # 字串 str string
+
+# 字串運算
+hello = 'hello world'
+print( hello  + "!" ) # 字串串接
+print( "100" + "1" ) # 字串串接
+# print( hello * "2" ) # 字串重複
+print( hello * 2 )  # 字串重複
+
+#
+input1 = int( input("請輸入整數: ") )   # input()輸入的資料型態是字串，需要轉型成整數int
+input2 = input( "請輸入整數: " )
+print( input1, int(input2) )
--- practice1.py
+++ practice1.py
@@ -1,9 +1,15 @@
-# 第一個練習
+# 第一個練習 - 變數計算
 # 晚爨1000元(未稅5%)，服務費1成，請問總共多少錢?
 price_dinner = 1000
 tax_rate = 0.05
 service_rate = 0.1
-tax_dinner = price_dinner * tax_rate
-service_dinner = price_dinner * service_rate
-total_dinner = price_dinner + tax_dinner + service_dinner
+service_dinner = price_dinner * ( 1 + service_rate )
+total_dinner = service_dinner * ( 1 + tax_rate )
 print("總共需要支付:", total_dinner, "元")
+
+# 第二個練習 - input輸入
+# 輸入出生國曆年份，輸出自己預計西元幾年退休(65歲)，輸出格式：“您的退休年份為”***"
+birth_year = int( input("請輸入您的出生國曆年份: ") )
+retiredAge = 65
+retiredYear = birth_year + retiredAge
+print("您的退休年份為", retiredYear)
