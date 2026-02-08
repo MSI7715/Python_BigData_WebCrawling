1f841809cf2ab731a5c73357da57b557b03ffd4c
Data_WH <git@example.com>
2026-02-08 14:04:01 +0800

    BigData_Lesson8_PM Stock Price requests

--- 2026_02_08/StockPrice_requests.py
+++ 2026_02_08/StockPrice_requests.py
@@ -2,5 +2,5 @@ import requests
 
 url = 'https://www.tpex.org.tw/www/zh-tw/afterTrading/tradingStock'
 payload = { 'code': '3630', 'date': '2026/02/01', 'id':'', 'response': 'json'}
-res = requests.post( url, data = payload )
+res = requests.post( url, data = payload, verify=False )
 print( res.text )
